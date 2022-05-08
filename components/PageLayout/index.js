6e72ca6a11973fcec8a195380474d5c1db7cb023
VishantRastogi <vishant777@gmail.com>
2022-05-08 18:47:50 +0530

    Init

---
+++ components/PageLayout/index.js
@@ -0,0 +1,22 @@
+import { Container } from "react-bootstrap";
+import NavBar from "components/Navbar";
+
+const PageLayout = ({ children, className }) => {
+  return (
+    <Container>
+      <NavBar />
+      <div className={`page-wrapper ${className}`}>{children}</div>
+      <footer className="page-footer">
+        <div>
+          <a href="#">courses</a>
+          {" | "}
+          <a href="#">github</a>
+          {" | "}
+          <a href="#">facebook</a>
+        </div>
+      </footer>
+    </Container>
+  );
+};
+
+export default PageLayout;
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { Row, Col } from "react-bootstrap";
+import PageLayout from "components/PageLayout";
+import AuthorIntro from "components/AuthorIntro";
+import CardListItem from "components/CardListItem";
+import CardItem from "components/CardItem";
+
+const Home = () => {
+  return (
+    <PageLayout className="">
+      <AuthorIntro />
+      <hr />
+      <Row className="mb-5">
+        <Col md="10">
+          <CardListItem />
+        </Col>
+        <Col md="4">
+          <CardItem />
+        </Col>
+      </Row>
+    </PageLayout>
+  );
+};
+
+export default Home;
